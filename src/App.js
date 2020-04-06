afe9107b53751cff453b41592673a94f32d99e53
aditya3131 <singhuday0007@gmail.com>
2020-04-06 10:41:05 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+     monsters : [
+       {
+        name : "Goku",
+        id: "r1c1"
+       },
+       {
+        name : "Vegeta",
+        id : "r1c2"
+       },
+       {
+        name : "Gohan",
+        id: "r1c3"
+       },
+     ]
+    }
+  }
 
+  render() {
+    return (
+      <div className="root" >
+        { this.state.monsters.map(ele => ( <h1 key={ele.id} > {ele.name }</h1> )
+       )}
+      </div>
+    );
+  }
+}
 export default App;
