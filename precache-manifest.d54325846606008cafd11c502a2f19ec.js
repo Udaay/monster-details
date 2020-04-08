83d83bf29b179ee94427f402118f4b431cbc51d1
Uday Singh <udays@MITRMUM.COM>
2020-04-08 15:19:36 +0530

    Updates

---
+++ precache-manifest.d54325846606008cafd11c502a2f19ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6446abe20972eeb00e8e91f2a6923d3",
+    "url": "/monster-details/index.html"
+  },
+  {
+    "revision": "d8c98246713bab5a4bff",
+    "url": "/monster-details/static/css/main.f462b93c.chunk.css"
+  },
+  {
+    "revision": "b84d4fa81fa13f79427d",
+    "url": "/monster-details/static/js/2.2e937bbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-details/static/js/2.2e937bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c98246713bab5a4bff",
+    "url": "/monster-details/static/js/main.e795bd37.chunk.js"
+  },
+  {
+    "revision": "ce610f4b1df81c88f786",
+    "url": "/monster-details/static/js/runtime-main.06ef4302.js"
+  }
+]);
